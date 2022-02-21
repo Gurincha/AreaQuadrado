ccb75f85021f7de8f09704d2df52ba060dfd8ee5
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-02-21 19:55:54 +0000

    att

--- Mensagem.java
+++ Mensagem.java
@@ -1,11 +1,20 @@
 import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Mensagem{
+   
     public static void main(String[] ags){
+        int num;
         int idade=18;
         double media= 7.55;
 
         System.out.println("jose tem "+idade+ "anos");
         System.out.printf("media = %.2f", media);
+
+       
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("digite um valor inteniro.");
+        num = entrada.nextInt();
+        System.out.println("pra que isso?"+num);
     }
 }
