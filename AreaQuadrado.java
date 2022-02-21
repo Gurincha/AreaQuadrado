0bd5e8864c711a70e9722f8108e1554b760058ae
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-02-21 19:39:19 +0000

    atualizado

--- AreaQuadrado.java
+++ AreaQuadrado.java
@@ -1,3 +1,3 @@
-public enum AreaQuadrado {
+public class AreaQuadrado {
     
 }
--- Mensagem.java
+++ Mensagem.java
@@ -2,7 +2,10 @@ import java.util.stream.Stream;
 
 public class Mensagem{
     public static void main(String[] ags){
-        System.out.println("Fatec Pompeia");
+        int idade=18;
+        double media= 7.55;
 
+        System.out.println("jose tem "+idade+ "anos");
+        System.out.printf("media = %.2f", media);
     }
 }
